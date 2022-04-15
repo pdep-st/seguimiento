903f808178bb9394792404595e30978884144264
Martín Gotelli Ferenaz <mgotelli@keepcon.com>
2022-04-15 16:19:30 -0300

    Agrega código clase 2

---
+++ seguimiento/2022/funcional/practica/clase2.hs
@@ -0,0 +1,36 @@
+-- alargarUnPoco/1: Recibe la longitud de un cable 
+-- y le agrega 10 metros
+
+
+-- sirve/1: Recibe la longitud de un cable y dice 
+-- si es mayor al largo maximo
+
+
+
+-- sirveAlargandoUnPoco/1
+
+
+
+-- signo/1
+
+
+-- esPar/1
+
+
+-- probabilidadLluvia/1 (clima)
+--   "Despejado"      0
+--   "Nublado"        50
+--   "Relampaguea"    100
+--   cualquierOtro    40
+
+
+
+
+-- buenDia/1
+-- "Lunes"  False
+-- "Martes" 0
+-- "Miércoles" "Ahí nomás"
+-- "Viernes" 'X'
+
+
+-- esMayorA10DespuesDeIncrementar/1
