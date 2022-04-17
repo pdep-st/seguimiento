3514c63823312e5251cbe0a04e449dca874b5af6
Martín Gotelli Ferenaz <mgotelli@keepcon.com>
2022-04-17 11:36:00 -0300

    Agrega link a video y apuntes, agrega código clase 2

--- seguimiento/2022/funcional/clase-2.md
+++ seguimiento/2022/funcional/clase-2.md
@@ -12,9 +12,16 @@
 
 ## Material utilizado
 
-* [Video de clase](pendiente)
+* [Video de clase](https://drive.google.com/file/d/1i_YP1gtoj2YJfnmfZ2kw1aU3WCuUgXUC/view?usp=sharing)
 * [Presentación clase](https://docs.google.com/presentation/d/17lf6CVezRo8hPKLNA2ZfPc1ultTBYY2sv4BjDKkEfRc)
 * [Código de clase](https://github.com/pdep-st/seguimiento/blob/main/seguimiento/2022/funcional/practica/clase2.hs)
+
+
+## Lectura Recomendada
+
+* [Apunte 1 - Guardas](https://docs.google.com/document/d/1W5BcOmIJMCylqAjqPw1RzPlujycbvNJueh8-Uyc2fMY/edit)
+* [Apunte 2 - Composición](https://docs.google.com/document/d/1n7TPE2qRpFSnj95lIZFD-q7Ko_DT9XZLH9_kEkNClrU/edit)
+* [Apunte 7 - Tipado](https://drive.google.com/open?id=1q2o2zCBU2LOfJs3nWG7-r6SaFHCIU5c0M4CJNmqOIO0)
 
 ## Para hacer después de la clase
 
--- seguimiento/2022/funcional/practica/clase2.hs
+++ seguimiento/2022/funcional/practica/clase2.hs
@@ -1,21 +1,34 @@
+import Text.Show.Functions
 -- alargarUnPoco/1: Recibe la longitud de un cable 
 -- y le agrega 10 metros
-
+alargarUnPoco longitud = longitud + 10
 
 -- sirve/1: Recibe la longitud de un cable y dice 
 -- si es mayor al largo maximo
-
+largoMaximo = 30
+sirve longitud = longitud > largoMaximo
 
 
 -- sirveAlargandoUnPoco/1
+-- sirveAlargandoUnPoco longitud = sirve (alargarUnPoco longitud)
+
+-- g(f(x))
 
 
+-- sirveAlargandoUnPoco longitud = (sirve . alargarUnPoco) longitud
+-- esPar = even
+
+sirveAlargandoUnPoco = sirve . alargarUnPoco
+-- (g o f)(x)
 
 -- signo/1
-
+signo numero
+    | numero > 0 = 1
+    | numero == 0 = 0
+    | otherwise = -1
 
 -- esPar/1
-
+esPar numero = mod numero 2 == 0
 
 -- probabilidadLluvia/1 (clima)
 --   "Despejado"      0
@@ -23,7 +36,11 @@
 --   "Relampaguea"    100
 --   cualquierOtro    40
 
-
+probabilidadLluvia clima
+    | clima == "Despejado" = 0
+    | clima == "Nublado" = 50
+    | clima == "Relampaguea" = 100
+    | otherwise = 40
 
 
 -- buenDia/1
@@ -31,6 +48,29 @@
 -- "Martes" 0
 -- "Miércoles" "Ahí nomás"
 -- "Viernes" 'X'
+-- buenDia dia
+--     | dia == "Lunes" = False
+--     | dia == "Martes" = 0
+--     | dia == "Miércoles" = "Ahí nomás"
+--     | dia == "Viernes" = 'X'
+
+identidad x = x
+
+-- esMayorA10DespuesDeIncrementar/1: después de sumarle 1 es mayor a 10
+-- esMayorA10DespuesDeIncrementar :: (Num a, Ord a) => a -> Bool
+esMayorA10DespuesDeIncrementar numero = (numero + 1) > 10
+
+-- fLoca :: (Num a, Ord a) => a -> a -> Bool
+fLoca x y = (x + 1) > y
+
+otraF x y
+    | x > "String" = "Chau"
+    | (y + 1) == 0 = "Hola"
+    | otherwise = "Para que no rompa"
 
 
--- esMayorA10DespuesDeIncrementar/1
+doble x = 2 * x
+
+-- tresVecesDoble x = doble (doble (doble x))
+-- tresVecesDoble x = (doble . doble . doble) x
+tresVecesDoble = doble . doble . doble
