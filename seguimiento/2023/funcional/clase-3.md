9f555a845e8bf0815e404924a6e918c7e0776041
Martín Gotelli Ferenaz <mgotelli@keepcon.com>
2023-04-15 12:48:42 -0300

    Clase 3

---
+++ seguimiento/2023/funcional/clase-3.md
@@ -0,0 +1,22 @@
+# Funcional - Clase 3
+
+## ¿Qué vamos a aprender?
+
+* Repaso de conceptos
+    * Tipado
+    * Composición
+    * Función como valor
+    * Guardas
+* Pattern Matching
+* Variable anónima
+* Tuplas
+
+## Material utilizado
+
+* Video de clase
+* [Presentación clase](https://docs.google.com/presentation/d/1D3Dd5YaNSMqP7gQd0QxkfjHEZEa-uMSYyXF5VQzreOE)
+* [Código de clase](https://github.com/pdep-st/seguimiento/blob/main/seguimiento/2023/funcional/practica/clase3.hs)
+
+## Para hacer después de la clase
+
+* [Repaso - Leer Apunte Modelado de Información (solo Tuplas)](https://docs.google.com/document/d/11C2UAbP70dP7sTID-ZxJm_a-5ypKxQUEuZr6GVk5yFI/edit#heading=h.x2xuqlkw85oe)
---
+++ seguimiento/2023/funcional/practica/clase3.hs
@@ -0,0 +1,51 @@
+import Text.Show.Functions
+
+
+--noMeImportaElPrimero
+
+-- esNombreLargo: un nombre es largo cuando excede
+-- los 7 caracteres
+
+
+-- Tipo de la (.)
+
+-- Definir la función (.)
+
+
+{-
+probabilidadLluvia clima
+    | clima == "Despejado" = 0
+    | clima == "Nublado" = 50
+    | clima == "Relampaguea" = 100
+    | otherwise = 40
+
+Cuando no es ninguna de las tres sumar un caso
+que sea 20 si la cantidad de letras es menor a 3
+que sea 40 si la cantidad de letras es mayor o igual a 3
+-}
+
+
+-- noMeImportaElPrimero
+-- noMeImportaElPrimero :: a -> b -> b
+
+
+-- factorial (5! = 5 * 4!, 0! = 1)
+
+-- dividir: un número por otro,
+-- obteniendo la parte entera y el resto
+
+-- esPar usando dividir
+
+-- punto: x, y
+
+-- nacimiento (año, mes, dia)
+
+-- persona: Nombre, Apellido, Edad
+
+
+-- auto: Marca, Modelo, Año
+
+
+-- cumplirAnios: Crecer un año
+
+-- tieneNombreLargo
