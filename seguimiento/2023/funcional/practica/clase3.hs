33a75a3aafdb6f160237259fd35646c535e8b51f
Martín Gotelli Ferenaz <mgotelli@keepcon.com>
2023-04-15 15:31:16 -0300

    Clase update

--- seguimiento/2023/funcional/practica/clase3.hs
+++ seguimiento/2023/funcional/practica/clase3.hs
@@ -1,16 +1,31 @@
 import Text.Show.Functions
+import Data.List
 
+-- (==) :: Eq a => a -> a -> Bool
 
 --noMeImportaElPrimero
 
 -- esNombreLargo: un nombre es largo cuando excede
 -- los 7 caracteres
+-- esNombreLargo nombre = length nombre > 7
+--excedeLargo:: Int -> Bool
+excedeLargo longitud = longitud > 7
 
+-- esNombreLargo:: (String -> Bool)
+esNombreLargo = excedeLargo . genericLength 
 
--- Tipo de la (.)
+-- Tipo de la (.) usando esNombreLargo
+-- Recibe dos parámetros (que son funciones que esperan un parámetro)
+-- La entrada del primer parámetro tiene que ser
+--  del mismo tipo que la salida del segundo parámetro
+-- Retorna una función que espera un parámetro
 
+-- (.) :: (b -> c) -> (a -> b) -> (a -> c)
+-- (excedeLargo . length)::
+--  (Int -> Bool) -> (String -> Int) -> (String -> Bool)
+
+(#) g f x = g (f x)
 -- Definir la función (.)
-
 
 {-
 probabilidadLluvia clima
@@ -28,7 +43,6 @@ que sea 40 si la cantidad de letras es mayor o igual a 3
 -- noMeImportaElPrimero
 -- noMeImportaElPrimero :: a -> b -> b
 
-
 -- factorial (5! = 5 * 4!, 0! = 1)
 
 -- dividir: un número por otro,
@@ -41,7 +55,6 @@ que sea 40 si la cantidad de letras es mayor o igual a 3
 -- nacimiento (año, mes, dia)
 
 -- persona: Nombre, Apellido, Edad
-
 
 -- auto: Marca, Modelo, Año
 
