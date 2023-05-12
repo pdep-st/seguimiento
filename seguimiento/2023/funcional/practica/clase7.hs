b73f1fbb120f8916fcf394d0c60d5f9a725992fb
Martín Gotelli Ferenaz <mgotelli@keepcon.com>
2023-05-12 19:39:20 -0300

    Clase 7

---
+++ seguimiento/2023/funcional/practica/clase7.hs
@@ -0,0 +1,41 @@
+import Text.Show.Functions
+
+-- esNombreLargo = (>7) . length
+
+-- reducir: para permitir la definición de
+-- funciones recursivas
+
+reducir:: (b -> a -> b) -> b -> [a] -> b
+reducir _ casoBase [] = casoBase
+
+reducir funcion casoBase (x:xs) =
+    funcion (reducir funcion casoBase xs) x
+
+-- sumatoria/1
+sumatoria [] = 0
+sumatoria (cabeza:cola) = cabeza + sumatoria cola
+
+-- productoria/1
+productoria [] = 1
+productoria (cabeza:cola) = cabeza * productoria cola
+
+-- longitud
+longitud (_:cola) = 1 + longitud cola
+longitud [] = 0
+
+-- maximum/minimum??
+
+-- restatoria??
+
+data Persona = Persona {
+    nombre :: String
+    } deriving (Eq, Show)
+
+personas = [Persona "Inia",
+    Persona "Martin",
+    Persona "Gaston"
+    ]
+
+-- todosLosNombres
+
+-- Golf con Lee Carvallo
