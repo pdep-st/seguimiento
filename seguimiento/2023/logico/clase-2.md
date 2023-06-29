84d31c5d940221631f6f103b652616f0c2251102
Martín Gotelli Ferenaz <mgotelli@keepcon.com>
2023-06-29 10:21:05 -0300

    Clase 2

---
+++ seguimiento/2023/logico/clase-2.md
@@ -0,0 +1,16 @@
+# Lógico - Clase 2
+
+## ¿Qué vamos a aprender?
+
+* Repaso general
+* `not/1` - Para verificar si no se cumple una consulta.
+  * Y aparecen los problemas con la inversibilidad.
+* `forall/2` Para verificar todos los individuos que cumplen la primer consulta además cumplan la segunda
+* Análisis de inversibilidad
+
+## Material utilizado
+
+* Video de la clase
+* [Presentación de la clase](https://docs.google.com/presentation/d/1Z-8ti2UCTVJdDBU2fmjV0Bvy6bcoKXhtXdXolbxewFg)
+* [Código de clase](https://github.com/pdep-st/seguimiento/blob/main/seguimiento/2023/logico/practica/clase2.pl)
+
---
+++ seguimiento/2023/logico/practica/clase2.pl
@@ -0,0 +1,48 @@
+/*
+ Tenemos los siguientes alumnos nicolas, malena, raul, carlos, santiago.
+ - Queremos saber:
+    - Si nicolas es alumno: ...
+    - Si santiago es alumno: ...
+    - Quienes son alumnos: ...
+    - Si tenemos algún alumno: ...
+*/
+
+
+/*
+ Tenemos las materias y sus anios
+    - algoritmos, analisis1 (1ro)
+    - pdep, sintaxis (2do)
+ - Queremos saber
+   - Si algoritmos esta en 2do: ...
+   - En que año esta algoritmos: ...
+   - Cuales son las materias de 2do: ...
+   - Materias de toda la carrera: ...
+*/
+
+
+% Queremos saber cuáles son los años (niveles) de la facu
+
+
+/*
+ Además de cada alumno conocemos la nota promedio de cada materia
+    - nicolas pdep 10
+    - nicolas proba 7
+    - nicolas sintaxis 8
+    - malena pdep 6
+    - raul pdep 9
+
+ - Queremos saber
+- Las notas de nicolas: ....
+   - Si rindió algún examen nico, pero en realidad podría ser cualquier alumno
+   - Qué materias aprobó malena
+    - Queremos poder hacerlo muchas veces esta tarea pero con diferentes alumnos
+   - esta al principio de la carrera si aún no rindió ningún examen
+*/
+
+
+/*
+   queremos saber si un alumno aprobo un anio
+      aprobo todas las materias de un anio
+        1. materias de un anio?
+        2. aprobo materia?
+*/
