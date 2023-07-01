8d1fd7f49b7f5b964f5027d167c95a4af22177ad
Martín Gotelli Ferenaz <mgotelli@keepcon.com>
2023-07-01 15:58:51 -0300

    Clase 2

--- seguimiento/2023/logico/practica/clase2.pl
+++ seguimiento/2023/logico/practica/clase2.pl
@@ -1,27 +1,49 @@
+profesor(martin).
+
 /*
  Tenemos los siguientes alumnos nicolas, malena, raul, carlos, santiago.
  - Queremos saber:
-    - Si nicolas es alumno: ...
-    - Si santiago es alumno: ...
-    - Quienes son alumnos: ...
-    - Si tenemos algún alumno: ...
+    - Si nicolas es alumno: ?- alumno(nicolas).
+    - Si santiago es alumno: ?- alumno(santiago).
+    - Quiénes son alumnos: ?- alumno(Alumno).
+    - Si tenemos algún alumno: ?- alumno(_).
 */
-
+% alumno/1
+alumno(nicolas).
+alumno(malena).
+alumno(raul).
+alumno(carlos).
+alumno(santiago).
+alumno(messi).
 
 /*
  Tenemos las materias y sus anios
     - algoritmos, analisis1 (1ro)
     - pdep, sintaxis (2do)
  - Queremos saber
-   - Si algoritmos esta en 2do: ...
-   - En que año esta algoritmos: ...
-   - Cuales son las materias de 2do: ...
-   - Materias de toda la carrera: ...
+   - Si algoritmos está en 2do: ?- materia(algoritmos, 2).
+   - En qué año está algoritmos: ?- materia(algoritmos, Año).
+   - Cuáles son las materias de 2do: ?- materia(Materia, 2).
+   - Materias de toda la carrera: ?- materia(Materia, Año).
 */
 
+% materia(Nombre, Anio)
+materia(algoritmos, 1).
+materia(analisis1, 1).
+materia(pdep, 2).
+materia(sintaxis, 2).
 
-% Queremos saber cuáles son los años (niveles) de la facu
+/*
+primero(algoritmos).
+primero(analisis1).
+segundo(pdep).
+segundo(sintaxis).
 
+anio(Materia, 1):- primero(Materia).
+anio(Materia, 2):- segundo(Materia).
+*/
+
+% Queremos saber cuáles son los años (niveles) de la facu: ?- materia(_, Año).
 
 /*
  Además de cada alumno conocemos la nota promedio de cada materia
@@ -32,13 +54,29 @@
     - raul pdep 9
 
  - Queremos saber
-- Las notas de nicolas: ....
-   - Si rindió algún examen nico, pero en realidad podría ser cualquier alumno
-   - Qué materias aprobó malena
+   - Las notas de nicolas: ?- nota(nicolas, Materia, Nota).
+   - Si rindió algún examen nico, pero en realidad podría ser cualquier alumno: ?- rindio(nicolas).
+   - Qué materias aprobó malena: ?- aprobo(malena, Materia).
     - Queremos poder hacerlo muchas veces esta tarea pero con diferentes alumnos
-   - esta al principio de la carrera si aún no rindió ningún examen
+   - Está al principio de la carrera si aún no rindió ningún examen
 */
+% nota(Alumno, Materia, Nota).
+nota(nicolas, pdep, 10).
+nota(nicolas, proba, 7).
+nota(nicolas, sintaxis, 8).
+nota(malena, pdep, 6).
+nota(raul, pdep, 9).
+nota(messi, fulbo, 10).
 
+rindio(Alumno):- nota(Alumno, _, _).
+
+aprobo(Alumno, Materia):-
+   nota(Alumno, Materia, Nota),
+   Nota >= 6.
+
+estaAlPrincipio(Alumno):-
+   alumno(Alumno),
+   not(rindio(Alumno)).
 
 /*
    queremos saber si un alumno aprobo un anio
