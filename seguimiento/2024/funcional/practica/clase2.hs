642cf9c96b8eedd7227ebd165ec7a6c0d8805205
Iniaki Lopetegui <iniakilopetegui@gmail.com>
2024-04-09 11:06:59 -0300

    clase 2 primera parte

--- seguimiento/2024/funcional/practica/clase2.hs
+++ seguimiento/2024/funcional/practica/clase2.hs
@@ -1,14 +1,28 @@
+import Text.Show.Functions
 -- alargarUnPoco/1: Recibe la longitud de un cable 
 -- y le agrega 10 metros
-
+alargarUnPoco longitud = longitud + 10
 
 -- sirve/1: Recibe la longitud de un cable y dice 
 -- si es mayor al largo maximo
+longitudMaxima = 25
 
+sirve longitud = longitud > longitudMaxima
 
 
 -- sirveAlargandoUnPoco/1
 
+--sirveAlargandoUnPoco longitud = sirve (alargarUnPoco longitud)
+sirveAlargandoUnPoco = sirve . alargarUnPoco
+
+-- g o f (x) = g(f(x))
+
+doble unNumero = unNumero * 2
+
+--tresVecesDoble unNumero = doble (doble (doble unNumero))
+tresVecesDoble = doble . doble . doble
+
+--esPar = even
 
 
 -- signo/1
