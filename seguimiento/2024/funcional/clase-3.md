6f1f26c8419bb5a7d9830e8725c9a4490e92bba0
MartinGotelli <martin.ferenaz@housecallpro.com>
2024-04-17 11:26:13 -0300

    Clase 3

--- seguimiento/2024/funcional/clase-3.md
+++ seguimiento/2024/funcional/clase-3.md
@@ -13,10 +13,11 @@
 
 ## Material utilizado
 
-* Video de clase
+* [Video de clase](https://youtu.be/mGqX0Mpy9IE)
 * [Presentación clase](https://docs.google.com/presentation/d/1DxUhgWaIfmK0ezIbmZb6bjDTSOxF0O6AvuJ_5TbA6BA)
 * [Código de clase](https://github.com/pdep-st/seguimiento/blob/main/seguimiento/2023/funcional/practica/clase3.hs)
 
 ## Para hacer después de la clase
 
 * [Repaso - Leer Apunte Modelado de Información (solo Tuplas)](https://docs.google.com/document/d/11C2UAbP70dP7sTID-ZxJm_a-5ypKxQUEuZr6GVk5yFI/edit#heading=h.x2xuqlkw85oe)
+* [Trabajo Práctico - Entrega #1](https://docs.google.com/document/d/1-qAQEW6TvQw4F7LeqhmXQ-gWQtovopsqiKarvHEiIHc)
--- seguimiento/2024/funcional/practica/clase3.hs
+++ seguimiento/2024/funcional/practica/clase3.hs
@@ -3,7 +3,6 @@ import Data.List
 
 --maximoDeTres :: Int -> Int
 maximoDeTres numero = max numero 3
---noMeImportaElPrimero
 
 --Quiero una funcion que cuando recibe un numero par, me devuelva el doble. 
 -- Si no, me devuelve el mismo numero
@@ -27,14 +26,10 @@ esNombreLargo2 = esMayorASiete # cantidadDeCaracteres
 
 (#) g f x = g ( f x )
 
-(#):: (b -> c) -> (a -> b) -> a -> c
---(#):: (b -> c) -> (a -> b) -> (a -> c)
-
 -- Tipo de la (.)
 
-
-
-
+(#):: (b -> c) -> (a -> b) -> a -> c
+--(#):: (b -> c) -> (a -> b) -> (a -> c)
 
 {-
 probabilidadLluvia clima
@@ -44,31 +39,66 @@ probabilidadLluvia clima
     | otherwise = 40
 Cuando no es ninguna de las tres sumar un caso
 que sea 20 si la cantidad de letras es menor a 3
-que sea 40 si la cantidad de letras es mayor o igual a 3
+que sea 30 si la cantidad de letras es exactamente 3
+que sea 40 si la cantidad de letras es mayor a 3
 -}
 
+-- probabilidadLluvia/1
+probabilidadLluvia "Despejado" = 0
+probabilidadLluvia "Nublado" = 50
+probabilidadLluvia "Relampaguea" = 100
+probabilidadLluvia clima
+    | length clima == 1 = 0
+    | length clima > 1 && length clima < 4 = 20
+    | length clima == 4 = 30
+    | otherwise = 40
 
 -- noMeImportaElPrimero
 -- noMeImportaElPrimero :: a -> b -> b
-
+noMeImportaElPrimero 10 _ = 800
+noMeImportaElPrimero _ x = x + 2
 
 -- factorial (5! = 5 * 4!, 0! = 1)
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
 
 -- dividir: un número por otro,
 -- obteniendo la parte entera y el resto
+dividir dividendo divisor = (div dividendo divisor, mod dividendo divisor)
 
 -- esPar usando dividir
+esPar numero = snd (dividir numero 2) == 0
 
 -- punto: x, y
 
+origen = (0, 0)
+puntazo = (1, 3)
+
 -- nacimiento (año, mes, dia)
+nacimientoValentina = (2005, 3, 23)
+
+anio (anio, _, _) = anio
+mes (_, mes, _) = mes
+dia (_, _, dia) = dia
 
 -- persona: Nombre, Apellido, Edad
-
+bauti :: Persona
+bauti = ("Bautista", "Leguía", 20)
 
 -- auto: Marca, Modelo, Año
+cronos = ("Fiat", "Cronos", 2022)
 
+type Persona = (String, String, Integer)
 
 -- cumplirAnios: Crecer un año
+edad (_, _, edad) = edad
+
+cursarUTN:: Persona -> Persona
+cursarUTN persona = 
+    (cumplirAnios . cumplirAnios . cumplirAnios . 
+      cumplirAnios . cumplirAnios . cumplirAnios) persona
+
+cumplirAnios (nombre, apellido, edad) = (nombre, apellido, edad + 1)
 
 -- tieneNombreLargo
