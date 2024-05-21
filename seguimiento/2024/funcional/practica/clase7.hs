3b62bd26d3aef8c8b859d091930c0803e039d34c
Iniaki Lopetegui <iniakilopetegui@gmail.com>
2024-05-21 10:35:51 -0300

    fold v1

--- seguimiento/2024/funcional/practica/clase7.hs
+++ seguimiento/2024/funcional/practica/clase7.hs
@@ -3,18 +3,46 @@ import Text.Show.Functions
 -- reducir: para permitir la definición de
 -- funciones recursivas
 
-
 -- sumatoria/1
-sumatoria [] = 0
-sumatoria (x:xs) = x + sumatoria xs
+sumatoria = reducirR (+) 0
 
 -- productoria/1
-productoria [] = 1
-productoria (x:xs) = x * productoria xs
+productoria = reducirR (*) 1
+
+concatenacion = reducirR (++) ""
 
 -- longitud/1
-longitud [] = 0
-longitud (_:xs) = 1 + longitud xs
+longitud = reducirR (\_ acumulador -> 1 + acumulador) 0
+
+reducirR _ semilla [] = semilla
+reducirR f semilla (x:xs) = f x (reducirR f semilla xs)
+-- (+) 8 ((+) 4 0))
+-- (+) 8 4
+--    12
+
+
+--foldl _ semilla [] = semilla
+--foldl f semilla (x:xs) = f (foldl f semilla xs) x
+-- foldl (+) 0 [8,4]
+-- (+) ((+) 0 4 ) 8
+-- (+) 4 8
+--    12
+
+{-- reducir (+) 0 [8,4]
+primera vuelta
+x=8 xs=[4]
+
+segunda vuelta
+x=4 xs= []
+
+tercera vuelta
+semilla = 0
+-}
+-- (+) 8 ((+) 4 0))
+-- (+) 8 4
+--    12
+
+
 
 
 -- maximum/minimum??
