fe52c17002ac73de1e9edfa3c2aabc24a765ac10
MartinGotelli <martin.ferenaz@housecallpro.com>
2024-05-23 12:42:46 -0300

    Clase 7

--- seguimiento/2024/funcional/practica/clase7.hs
+++ seguimiento/2024/funcional/practica/clase7.hs
@@ -54,14 +54,34 @@ semilla = 0
 -- maximum/minimum??
 
 maximo (x:xs) = foldl max x xs
---maximo' (x:xs) = foldl1 max xs
+maximo':: Ord a => [a] -> a
+maximo' = foldl1 max
 
 minimo (x:xs) = foldl min x xs
+minimo':: Ord a => [a] -> a
+minimo' = foldl1 min
 
-sumatoriaSinListaVacia (x:xs) = foldl (+) x xs
+
 
 -- restatoria??
 
+foldL _ semilla [] = semilla
+--foldL f semilla (x:xs) = f (foldL f semilla xs) x
+foldL f acumulador (x:xs) = foldL f (f acumulador x) xs
+
+-- foldL (++) "" ["a", "b"]
+-- foldL (++) ((++) "" "a") ["b"] -> foldL (++) "a" ["b"]
+-- foldL (++) ((++) "a" "b") [] -> foldL (++) "ab" []
+-- "ab"
+
+{- 
+reducirL _ semilla [] = semilla
+reducirL f semilla (x:xs) = f (reducirL f semilla xs) x
+
+(++) (reducirL (++) "" ["b"]) "a"
+(++) ((++) (reducirL (++) "" []) "b") "a"
+(++) ((++) "" "b") "a"
+-}
 
 data Persona = Persona {
     nombrePersona :: String
