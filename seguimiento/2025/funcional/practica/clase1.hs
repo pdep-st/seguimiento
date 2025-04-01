261fc5af3e1b20053d3591da8104bd45e364d8f5
MartinGotelli <martin.ferenaz@housecallpro.com>
2025-04-01 09:11:45 -0300

    Clase 1

---
+++ seguimiento/2025/funcional/practica/clase1.hs
@@ -0,0 +1,17 @@
+import Text.Show.Functions
+
+doble x = 2 * x
+
+esPar:: (Int -> Bool)
+esPar = even
+
+profe:: String
+--profe = "Iña"
+profe = "Martín"
+
+suma:: Int -> Int -> Int
+suma x y = x + y
+
+iña = "Hola"
+
+sumarTres x y z = x + y + z
