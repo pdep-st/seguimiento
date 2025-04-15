28f43b57bb315aad3849785041360309d55c7190
BautiL <131457048+BautiL@users.noreply.github.com>
2025-04-15 12:39:03 -0300

    enunciado

--- seguimiento/2025/funcional/practica/clase4.hs
+++ seguimiento/2025/funcional/practica/clase4.hs
@@ -1,10 +1,11 @@
 import Text.Show.Functions
+import Data.List
 
 type Persona = (String, Int)
 
 martina = ("Martina", 21)
 -- nombre (x, y) = fst (x, y)
-nombre persona = fst persona
+--nombre persona = fst persona
 cumplirAnios (nombre, edad) = (nombre, edad + 1)
 
  -- Cómo serán las raíces de mi cuadrática
@@ -76,3 +77,89 @@ flip' f x y = f y x
 
 -- aplicar y = 1, z = 3 
 -- (flip ( flip fLoca 1) 3) 2
+
+lista1 = [10, 30, 34]
+lista2 = [23, 56, 30]
+
+-- Hora de aventura
+
+type Libro = (String, String, Double)
+
+elVisitante :: Libro
+elVisitante = ("El Visitante", "Stephen King", 592)
+
+--shingekiNoKyojin tomo = ("Shingeki no Kyojin " ++ show tomo, "Hajime Isayama", 40)
+
+shingekiNoKyojin1 :: Libro
+shingekiNoKyojin1 = ("Shingeki No Kyojin cap 1", "Hajime Isayama", 40)
+
+shingekiNoKyojin3 :: Libro
+shingekiNoKyojin3 = ("Shingeki no Kyojin 3", "Hajime Isayama", 40)
+
+shingekiNoKyojin127 :: Libro
+shingekiNoKyojin127 = ("Shingeki no Kyojin 127", "Hajime Isayama", 40)
+
+fundacion :: Libro
+fundacion = ("Fundacion", "Isaac Asimov", 230)
+
+sandman5 :: Libro
+sandman5 = ("sandman5", "Neil Gaiman", 35)
+
+sandman10 :: Libro
+sandman10 = ("sandman10", "Neil Gaiman", 35)
+
+sandman12 :: Libro
+sandman12 = ("sandman12", "Neil Gaiman", 35)
+
+eragon :: Libro
+eragon = ("eragon", "Christopher Paolini", 544)
+
+eldest :: Libro
+eldest = ("eldest", "Christopher Paolini", 704)
+
+brisignr :: Libro
+brisignr = ("brisignr", "Christopher Paolini", 700)
+
+legado :: Libro
+legado = ("legado", "Christopher Paolini", 811)
+
+type Biblioteca = [Libro]
+
+bibliotecaPdeP :: Biblioteca
+bibliotecaPdeP = [
+        elVisitante,
+        shingekiNoKyojin1,
+        shingekiNoKyojin3,
+        shingekiNoKyojin127,
+        fundacion,
+        sandman5,
+        sandman10,
+        sandman12,
+        eragon,
+        eldest, 
+        brisignr, 
+        legado
+    ]
+
+sagaEragon = [eragon,
+        eldest, 
+        brisignr, 
+        legado]
+
+promedioDePaginas biblioteca = cantidadPaginas biblioteca `div` cantidadLibros biblioteca
+
+paginas (_,_,paginas) = paginas
+
+cantidadPaginas biblioteca = sum(map paginas biblioteca)
+
+cantidadLibros = length
+
+esLecturaObligatoria libro = autor libro == "Stephen King" || esDeEragon libro || nombre libro == "Fundacion"
+
+autor (_, autor, _) = autor
+
+esDeEragon libro = elem libro sagaEragon 
+
+nombre (nombre, _, _) = nombre
+
+sonLecturaObligatoria biblioteca = filter esLecturaObligatoria biblioteca
