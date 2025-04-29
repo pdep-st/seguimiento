04f6b9c58c81b5ad8a88564bdbd4ef7194bb932a
Iniaki Lopetegui <iniakilopetegui@gmail.com>
2025-04-29 14:00:05 -0300

    pattern matching listas

--- seguimiento/2025/funcional/practica/clase6.hs
+++ seguimiento/2025/funcional/practica/clase6.hs
@@ -1,13 +1,42 @@
 -- cabeza, cola
- 
+ cabeza (cabeza:_) = cabeza
+
+ cola (_:cola) = cola
+
+
  -- estaVacia
+ estaVacia (_:_) = False
+ estaVacia [] = True
+
+
+ --tieneExactamenteUnSoloElemento
+ --tieneUnSoloElemento' (_:[]) = True
+ --tieneUnSoloElemento' (_:_) = False
+ --tieneUnSoloElemento' [] = False
  
- --tieneUnSoloElemento
- 
+ tieneUnSoloElemento [_] = True
+ tieneUnSoloElemento _ = False
+
+
  --estaOrdenada
- 
+
+
  -- longitud
+ -- [] -> 0
+ -- [3] -> 1
+ -- [3,4] -> 2
+
+ longitud (_:xs) = 1 + longitud xs
+ longitud [] = 0
+
  
  -- sumatoria/1
+ -- [1,2,3] -> 6
+ sumatoria [x] = x
+ sumatoria (x:xs) = x + sumatoria xs
  
+
  -- productoria/1
+  -- [1,2,3,4] -> 24
+ productoria [x] = x
+ productoria (x:xs) = x * productoria xs 
