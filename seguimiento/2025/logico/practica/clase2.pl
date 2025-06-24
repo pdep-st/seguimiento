f84148f7ed64cf273be00495628b8138e62251aa
Iniaki Lopetegui <iniakilopetegui@gmail.com>
2025-06-24 10:57:43 -0300

    alumnos

--- seguimiento/2025/logico/practica/clase2.pl
+++ seguimiento/2025/logico/practica/clase2.pl
@@ -7,6 +7,12 @@
     - Si tenemos algún alumno: ...
 */
 
+alumno(nicolas).
+alumno(malena).
+alumno(raul).
+alumno(carlos).
+alumno(santiago).
+
 
 /*
  Tenemos las materias y sus anios
@@ -19,9 +25,21 @@
    - Materias de toda la carrera: ...
 */
 
+%primerAnio(algoritmos, analisis).
+%segundoAnio(pdep, sintaxis).
+
+% materia/2
+materia(algoritmos, 1).
+materia(analisis1, 1).
+materia(pdep, 2).
+materia(sintaxis, 2).
+
+% segundo(Materia):- materia(Materia, 2). 
+
+
 
 % Queremos saber cuáles son los años (niveles) de la facu
-
+anio(Anio) :- materia(_, Anio).
 
 /*
  Además de cada alumno conocemos la nota promedio de cada materia
@@ -36,8 +54,24 @@
    - Si rindió algún examen nico, pero en realidad podría ser cualquier alumno
    - Qué materias aprobó malena
     - Queremos poder hacerlo muchas veces esta tarea pero con diferentes alumnos
-   - esta al principio de la carrera si aún no rindió ningún examen
-*/
+   - Queremos saber si un alumno esta al principio de la carrera, esto es cuando aún no rindió ningún examen
+*/       
+ nota(nicolas, pdep, 10).
+ nota(nicolas, proba, 7).
+ nota(nicolas, sintaxis, 8).
+ nota(malena, pdep, 6).
+ nota(raul, pdep, 9).
+
+rindio(Alumno):- nota(Alumno,_,_).
+aprobo(Alumno, Materia):- nota(Alumno,Materia,Nota), Nota >= 6.
+
+% Inversibilidad
+estaAlPrincipio(Alumno) :- 
+   alumno(Alumno), % generador
+   not(rindio(Alumno)).
+   
+
+% existe(_).
 
 
 /*
