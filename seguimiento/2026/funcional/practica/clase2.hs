fc73e2d4a90ed86e2fc87903825e7d7703746099
BautiL <bleguia@frba.utn.edu.ar>
2026-04-14 10:43:17 -0300

    composicion

--- seguimiento/2026/funcional/practica/clase2.hs
+++ seguimiento/2026/funcional/practica/clase2.hs
@@ -3,4 +3,29 @@ import PdePreludat
 triple :: Number -> Number
 triple numero = numero * 3
 
+doble :: Number -> Number
+doble numero = numero * 2
+
+sextuple numero = doble(triple(numero))
+
 fLoca numero otroNumero = (numero + otroNumero) * 3
+
+-- alargarUnPoco/1: Recibe la longitud de un cable 
+-- y le agrega 10 metros
+
+alargarUnPoco :: Number -> Number
+alargarUnPoco longitud = longitud + 10
+
+-- sirve/1: Recibe la longitud de un cable y dice 
+-- si es menor al largo maximo
+
+largoMaximo = 100
+
+sirve :: Number -> Bool
+sirve longitud = longitud < largoMaximo
+
+-- sirveAlargandoUnPoco/1
+-- nos indica si una longitud es util luego de alargarla 10 metros
+
+sirveAlargandoUnPoco :: Number -> Bool
+sirveAlargandoUnPoco longitud = (sirve . alargarUnPoco) longitud
