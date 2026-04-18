4691a6dfb0002866eb90b937007b52472f97b936
MartinGotelli <martin.ferenaz@housecallpro.com>
2026-04-18 10:47:12 -0300

    Preparo clase 3

---
+++ seguimiento/2026/funcional/clase-3.md
@@ -0,0 +1,23 @@
+# Funcional - Clase 3
+
+## ¿Qué vamos a aprender?
+
+* Repaso de conceptos
+    * Tipado
+    * Composición
+    * Función como Valor
+    * Guardas
+* Pattern Matching
+* Variable Anónima
+* Tuplas
+
+## Material utilizado
+
+* [Video de clase]()
+* [Presentación clase](https://docs.google.com/presentation/d/14p1Z-1-0YBlx60Okh7Bpz4YNoaPL-SntqTtdz-QFCfg/)
+* [Código de clase](https://github.com/pdep-st/seguimiento/blob/main/seguimiento/2026/funcional/practica/clase3.hs)
+
+## Para hacer después de la clase
+
+* [Repaso - Leer Apunte Modelado de Información (solo Tuplas)](https://docs.google.com/document/d/11C2UAbP70dP7sTID-ZxJm_a-5ypKxQUEuZr6GVk5yFI/edit#heading=h.x2xuqlkw85oe)
+* [Trabajo Práctico - Entrega #1](https://docs.google.com/document/d/1y6qCKT-VwOG-VOVVjusIr1ZI0ZqPdQhaPanlPNp9lVQ)
---
+++ seguimiento/2026/funcional/practica/clase3.hs
@@ -0,0 +1,53 @@
+import PdePreludat
+
+--Quiero una funcion que cuando recibe un numero par, me devuelva el doble. 
+-- Si no, me devuelve el mismo numero
+
+
+-- esNombreLargo: un nombre es largo cuando excede
+-- los 7 caracteres
+
+--esNombreLargo' nombre = esMayorASiete (cantidadDeCaracteres nombre)
+
+
+-- Definir la función (#)
+
+-- Tipo de la (.)
+
+
+{-
+Cuando no es ninguna de las tres sumar un caso
+que sea 9 por cada letra si la cantidad de letras es menor a 3
+que sea 30 si la cantidad de letras es exactamente 3
+que sea 6 por cada letra si la cantidad de letras es mayor a 3
+-}
+
+-- probabilidadLluvia/1
+
+
+-- noMeImportaElPrimero
+
+-- fLoca: Cuando x es 0, es la suma de y dos veces con z si z es mayor a 10, si no, es y tres veces sola
+-- Cuando x no es 0, es x + y + z
+
+
+-- factorial (5! = 5 * 4!, 0! = 1)
+
+
+-- dividir: un número por otro,
+-- obteniendo la parte entera y el resto
+
+-- esPar usando dividir
+
+-- punto: x, y
+
+
+-- nacimiento (año, mes, dia)
+
+-- persona: Nombre, Apellido, Edad
+
+-- auto: Marca, Modelo, Año
+
+-- cumplirAnios: Crecer un año
+
+-- tieneNombreLargo
