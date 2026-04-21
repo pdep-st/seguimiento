3b7e95de965d3936cd2263e0804d4a063ca1b4e6
mati <matias.dimascio@gmail.com>
2026-04-21 10:50:29 -0300

    clase3-patternMatching

--- seguimiento/2026/funcional/practica/clase3.hs
+++ seguimiento/2026/funcional/practica/clase3.hs
@@ -16,23 +16,39 @@ import PdePreludat
 
 
 {-
+probabilidadLluvia clima
+    | clima == "Despejado" = 0
+    | clima == "Nublado" = 50
+    | clima == "Relampaguea" = 100
+    | otherwise = 40
 Cuando no es ninguna de las tres sumar un caso
 que sea 9 por cada letra si la cantidad de letras es menor a 3
-que sea 30 si la cantidad de letras es exactamente 3
 que sea 6 por cada letra si la cantidad de letras es mayor a 3
+que sea 30 si la cantidad de letras es exactamente 3
 -}
 
 -- probabilidadLluvia/1
-
+probabilidadLluvia "Despejado" = 0
+probabilidadLluvia "Nublado" = 50
+probabilidadLluvia "Relampaguea" = 100
+probabilidadLluvia clima
+    | length clima < 3 = 9 * length clima
+    | length clima > 3 = 6 * length clima
+    | otherwise = 30
 
 -- noMeImportaElPrimero
+noMeImportaElPrimero _ x = x
 
 -- fLoca: Cuando x es 0, es la suma de y dos veces con z si z es mayor a 10, si no, es y tres veces sola
 -- Cuando x no es 0, es x + y + z
-
+fLoca 0 y z
+    | z > 10 = 2 * y + z
+    | otherwise = 3 * y
+fLoca x y z = x + y + z
 
 -- factorial (5! = 5 * 4!, 0! = 1)
-
+factorial 0 = 1
+factorial numero = factorial (numero - 1) * numero
 
 -- dividir: un número por otro,
 -- obteniendo la parte entera y el resto
