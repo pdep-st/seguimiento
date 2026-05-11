6a52aef668a56ce9e8a9a67a0769be709606eec0
BautiL <bleguia@frba.utn.edu.ar>
2026-05-11 11:26:14 -0300

    clase 6 template

---
+++ seguimiento/2026/funcional/practica/clase6.hs
@@ -0,0 +1,29 @@
+import PdePreludat
+
+-- cabeza, cola
+
+
+ -- estaVacia
+
+
+ --tieneExactamenteUnSoloElemento
+ --tieneUnSoloElemento' (_:[]) = True
+ --tieneUnSoloElemento' (_:_) = False
+ --tieneUnSoloElemento' [] = False
+
+
+ --estaOrdenada
+
+
+ -- longitud
+ -- [] -> 0
+ -- [3] -> 1
+ -- [3,4] -> 2
+
+ 
+ -- sumatoria/1
+ -- [1,2,3] -> 6
+ 
+
+ -- productoria/1
+  -- [1,2,3,4] -> 24
