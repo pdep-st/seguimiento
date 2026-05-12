77f52990312fe22d2f6051af18cf8b5877db780e
MartinGotelli <martin.ferenaz@housecallpro.com>
2026-05-12 12:32:06 -0300

    Codigo clase 6

--- seguimiento/2026/funcional/practica/clase6.hs
+++ seguimiento/2026/funcional/practica/clase6.hs
@@ -1,17 +1,40 @@
+module Library where
 import PdePreludat
+
+funcionesCopadas = [even, odd, (>0)]
+
+cualesCumplen:: [(Number -> Bool)] -> Number -> [(Number -> Bool)] 
+cualesCumplen funciones valor =
+    --filter (\funcion -> funcion valor) funciones
+    filter ($ valor) funciones
+
+
+ -- tipo de la lambda :: (Number -> Bool) -> Bool
+
+aplicar funcion valor = funcion valor
+(#) funcion valor = funcion valor
+
+aplicar2 = (#2)
 
 -- cabeza, cola
 
+cabeza (x:_) = x
+cola (_:xs) = xs
 
  -- estaVacia
-
+estaVacia [] = True
+estaVacia _ = False
 
  --tieneExactamenteUnSoloElemento
- --tieneUnSoloElemento' (_:[]) = True
- --tieneUnSoloElemento' (_:_) = False
- --tieneUnSoloElemento' [] = False
+--tieneExactamenteUnSoloElemento (_:[]) = True
+tieneExactamenteUnSoloElemento [_] = True
+tieneExactamenteUnSoloElemento _ = False
 
+tieneExactamenteTres [_, _, _] = True
+tieneExactamenteTres _ = False
 
+tieneAlMenosDos (_:_:_) = True
+tieneAlMenosDos _ = False
  --estaOrdenada
 
 
@@ -19,11 +42,19 @@ import PdePreludat
  -- [] -> 0
  -- [3] -> 1
  -- [3,4] -> 2
-
+longitud :: [a] -> Number
+longitud [] = 0
+longitud (_:xs) = 1 + longitud xs
  
  -- sumatoria/1
  -- [1,2,3] -> 6
+sumatoria :: [Number] -> Number
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
  
 
  -- productoria/1
   -- [1,2,3,4] -> 24
+productoria :: [Number] -> Number
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
