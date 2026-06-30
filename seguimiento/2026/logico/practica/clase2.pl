7ca59ff9e21aaae6fb4284392bd4c250e7af36d7
MartinGotelli <martin.ferenaz@housecallpro.com>
2026-06-30 10:31:27 -0300

    Clase 2

--- seguimiento/2026/logico/practica/clase2.pl
+++ seguimiento/2026/logico/practica/clase2.pl
@@ -1,28 +1,40 @@
 /*
  Tenemos los siguientes alumnos nicolas, malena, raul, carlos, santiago.
  - Queremos saber:
-    - Si nicolas es alumno: ...
-    - Si santiago es alumno: ...
-    - Quienes son alumnos: ...
-    - Si tenemos algún alumno: ...
+    - Si nicolas es alumno: alumno(nicolas).
+    - Si santiago es alumno: alumno(santiago).
+    - Quienes son alumnos: alumno(Alumno).
+    - Si tenemos algún alumno: alumno(_).
 */
-
+alumno(nicolas).
+alumno(malena).
+alumno(raul).
+alumno(carlos).
+alumno(santiago).
 
 /*
  Tenemos las materias y sus anios
     - algoritmos, analisis1 (1ro)
     - pdep, sintaxis (2do)
  - Queremos saber
-   - Si algoritmos esta en 2do: ...
-   - En que año esta algoritmos: ...
-   - Cuales son las materias de 2do: ...
-   - Materias de toda la carrera: ...
+   - Si algoritmos esta en 2do: materia(algoritmos, 2).
+   - En que año esta algoritmos: materia(algoritmos, Año).
+   - Cuales son las materias de 2do: materia(Materia, 2).
+   - Materias de toda la carrera: materia(Materia, _).
 */
-
+% materia/2 -> materia(Materia, Año)
+materia(analisis1, 1).
+materia(algoritmos, 1).
+materia(pdep, 2).
+materia(sintaxis, 2).
 
 
 % Queremos saber cuáles son los años (niveles) de la facu
-
+año(1).
+año(2).
+año(3).
+año(4).
+año(5).
 
 /*
  Además de cada alumno conocemos la nota promedio de cada materia
@@ -33,14 +45,26 @@
     - raul pdep 9
 
  - Queremos saber
-- Las notas de nicolas: ....
+- Las notas de nicolas: nota(nicolas, Materia, Nota).
    - Si rindió algún examen nico, pero en realidad podría ser cualquier alumno
    - Qué materias aprobó malena
     - Queremos poder hacerlo muchas veces esta tarea pero con diferentes alumnos
    - Queremos saber si un alumno esta al principio de la carrera, esto es cuando aún no rindió ningún examen
 */       
 
+% nota/3 -> nota(Alumno, Materia, Nota).
+nota(nicolas, pdep, 10).
+nota(nicolas, proba, 7).
+nota(nicolas, sintaxis, 8).
+nota(malena, pdep, 6).
+nota(raul, pdep, 9).
+nota(malena, sintaxis, 3).
 
+rindio(Alumno):- nota(Alumno, _, _).
+
+aprobo(Alumno, Materia):-
+   nota(Alumno, Materia, Nota),
+   Nota >= 6.
 
 % Inversibilidad
 
